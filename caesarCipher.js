9975ea7e67f80b8482cdf0d6bcc3e7ad8b70a0eb
mdamian9 <m.damian9@hotmail.com>
2019-01-13 02:47:30 -0800

    Set up ceaserCipher / completed romanNumeralConverted.js

---
+++ caesarCipher.js
@@ -0,0 +1,10 @@
+function rot13(str) { // LBH QVQ VG!
+
+    const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+        "X", "Y", "Z"];
+    
+    return str;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,75 +1,16 @@
 convertToRoman = (num) => {
-
-    const numArr = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
-    const romanNumArr = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
-
-    const numStr = String(num);
-    const numDigits = numStr.length, largestPlace = numStr.length;
-    console.log(numDigits);
-    let numBreakDown = [];
-    for (let i = 0; i < numStr.length; i++) {
-        let convertedNumAtI;
-        switch (numStr.length) {
-            case 1:
-                convertedNumAtI = parseFloat(numStr[i]);
-                numBreakDown.push(convertedNumAtI);
-                break;
-            case 2:
-                if (i === 0) {
-                    convertedNumAtI = parseFloat(numStr[i] * 10);
-                    numBreakDown.push(convertedNumAtI);
-                } else {
-                    convertedNumAtI = parseFloat(numStr[i]);
-                    numBreakDown.push(convertedNumAtI);
-                };
-                break;
-            case 3:
-                switch (i) {
-                    case 0:
-                        convertedNumAtI = parseFloat(numStr[i] * 100);
-                        numBreakDown.push(convertedNumAtI);
-                        break;
-                    case 1:
-                        convertedNumAtI = parseFloat(numStr[i] * 10);
-                        numBreakDown.push(convertedNumAtI);
-                        break;
-                    case 2:
-                        convertedNumAtI = parseFloat(numStr[i]);
-                        numBreakDown.push(convertedNumAtI);
-                        break;
-                };
-                break;
-            case 4:
-            console.log(num);
-                switch (i) {
-                    case 0:
-                    console.log(i);
-                        convertedNumAtI = parseFloat(numStr[i] * 1000);
-                        numBreakDown.push(convertedNumAtI);
-                        break;
-                    case 1:
-                    console.log(i);
-                        convertedNumAtI = parseFloat(numStr[i] * 100);
-                        numBreakDown.push(convertedNumAtI);
-                        break;
-                    case 2:
-                    console.log(i);
-                        convertedNumAtI = parseFloat(numStr[i] * 10);
-                        numBreakDown.push(convertedNumAtI);
-                        break;
-                    case 4:
-                    console.log(i);
-                        convertedNumAtI = parseFloat(numStr[i]);
-                        numBreakDown.push(convertedNumAtI);
-                        break;
-                };
-                break;
+    var numerals = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+    var convertedNum = '';
+    var i;
+    for (i in numerals) {
+        while (num >= numerals[i]) {
+            convertedNum += i;
+            num -= numerals[i];
         };
-        // numBreakDown.push(parseInt(numStr[i]));
     };
-    console.log(numBreakDown);
-
-    return num;
+    return convertedNum;
 };
 
-convertToRoman(3254);
+console.log(convertToRoman(3));
+console.log(convertToRoman(333));
+console.log(convertToRoman(36));
