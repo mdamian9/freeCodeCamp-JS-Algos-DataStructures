b95d8cfc6d49bd3d12fcf425f1c52b3e9073708c
mdamian9 <m.damian9@hotmail.com>
2019-01-13 21:17:14 -0800

    Completed caesarCipher.js algorithm

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,10 +1,27 @@
-function rot13(str) { // LBH QVQ VG!
+rot13 = (str) => { // LBH QVQ VG!
 
-    const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
-        "X", "Y", "Z"];
-    
-    return str;
-}
+    const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let decoded = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (!str[i].match(/[a-z]/i)) {
+            decoded += str[i];
+        } else {
+            if (str[i] !== " ") {
+                if (letters.indexOf(str[i]) >= 13) {
+                    decoded += letters[letters.indexOf(str[i]) - 13];
+                } else {
+                    decoded += letters[letters.indexOf(str[i]) + 13];
+                };
+            } else {
+                decoded += " ";
+            };
+        };
+    };
+    return decoded;
+};
 
 // Change the inputs below to test
-rot13("SERR PBQR PNZC");
+console.log(rot13("SERR CVMMN!"));
+console.log(rot13("SERR YBIR?"));
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
