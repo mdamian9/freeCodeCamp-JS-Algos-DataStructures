3aff02bc2d557b9ea4af9ca721a3d351dfa63834
mdamian9 <m.damian9@hotmail.com>
2019-01-14 02:18:42 -0800

    Tested on FCC, all tests passed. Completed phoneNumValidator algorithm

--- phoneNumValidator.js
+++ phoneNumValidator.js
@@ -1,37 +1,53 @@
 telephoneCheck = (str) => {
-    let flag;
-    if (str.length >= 10 && str.length <= 16) {
+    let flag, p = 0, d = 0;
+    if (str.length > 9 && str.length < 17) {
         for (let i = 0; i < str.length; i++) {
-            if (!str[i].match(/[0-9]/i)) {
+            if (!str[i].match(/[0-9\-\(\)\s]/i)) {
                 flag = false;
+                break;
             } else {
-                if (!str[i].match(/[0-9\-\(\)]/i)) {
-                    flag = false;
-                    break;
-                } else {
-                    if (str[1] === " ") {
-                        if (str[0] !== 1) {
-                            flag = false;
-                        };
-                    } else {
+                if (str[i].match(/[\(\)]/i)) {
+                    p++;
+                };
+                if (str[1] === " ") {
+                    if (str[0] === "1") {
                         flag = true;
-                    }
+                    } else {
+                        flag = false;
+                    };
+                } else {
+                    if (!str[i].match(/[0-9\-\(]/i)) {
+                        flag = false;
+                    } else {
+                        if (str[i].match(/[\-]/i)) {
+                            d++;
+                            if (str.length === 13 && d === 0) {
+                                flag = false;
+                            } else {
+                                flag = true;
+                            };
+                        };
+                    };
+                };
+                if (p > 0 && p != 2) {
+                    flag = false;
+                }
+                if (p == 2 && str[0] != 1 && str[0] !== "(") {
+                    flag = false;
+                };
+                if (str.length === 10 && str.match(/[0-9]/i)) {
+                    flag = true;
                 };
             };
         };
+        if (str.length === 11 && str[0] !== 1) {
+            flag = false;
+        };
+    } else {
+        flag = false;
     };
     console.log(flag);
     return flag;
 };
 
-telephoneCheck("1 555-555-5555");
-telephoneCheck("1 (555) 555-5555");
-telephoneCheck("1 555 555 5555");
-telephoneCheck("1 456 789 4444");
-telephoneCheck("(6054756961)");
-telephoneCheck("27576227382");
-telephoneCheck("(275)76227382");
-telephoneCheck("2(757)6227382");
-telephoneCheck("2(757)622-7382");
-telephoneCheck("555)-555-5555");
-telephoneCheck("(555-555-5555");
+telephoneCheck("5555555555");
