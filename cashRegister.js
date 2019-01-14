0038c53d90655d73b49855f62f49ff866766f5cf
mdamian9 <m.damian9@hotmail.com>
2019-01-14 15:27:39 -0800

    Progress on cashRegister.js algorithm

--- cashRegister.js
+++ cashRegister.js
@@ -1,8 +1,23 @@
 checkCashRegister = (price, cash, cid) => {
-    var change = {};
-    change.id = "id";
-    console.log(change.id);
-    return change;
+    let cidSum = 0;
+    let changeObj = {};
+    cid.forEach((arr) => {
+        cidSum += arr[1];
+    });
+    cidSum = parseFloat(cidSum.toFixed(2));
+    const change = cash - price;
+    if (cidSum < change) {
+        changeObj.id = "INSUFFICIENT_FUNDS";
+        changeObj.change = [];
+    } else if (cidSum === change) {
+        changeObj.id = "CLOSED";
+        changeObj.change = [];
+    } else {
+        changeObj.id = "OPEN";
+        changeObj.change = [];
+    };
+    console.log(changeObj);
+    return changeObj;
 };
 
 checkCashRegister(19.5, 20,
